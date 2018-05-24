070c383afbaf0ea438f1005bcf4709ba2f66a8e9
Kriyen Padayachee <kriyenkp@gmail.com>
2018-05-24 16:17:15 +0200

    Update to work with Myo

--- app/src/main/java/com/example/mpv/bb8/MainActivity.java
+++ app/src/main/java/com/example/mpv/bb8/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mpv.bb8;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
+
+import android.content.Context;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -22,101 +24,26 @@ import com.orbotix.common.internal.DeviceResponse;
 import com.orbotix.joystick.api.JoystickEventListener;
 import com.orbotix.joystick.api.JoystickView;
 
+
 import com.thalmic.myo.AbstractDeviceListener;
-import com.thalmic.myo.Arm;
 import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
 import com.thalmic.myo.Pose;
-import com.thalmic.myo.Quaternion;
-import com.thalmic.myo.XDirection;
+import com.thalmic.myo.scanner.ScanActivity;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends Activity {
-    private static final String TAG = "BackgroundService";
-    private Toast mToast;
 
-    private TextView mLockStateView;
-    private TextView mTextView;
+
+
+    private String TAG = "MyO Test";
+
+    Context context;
+
     private DeviceListener mListener;
-
-    {
-        mListener = new AbstractDeviceListener() { 
-
-            // onConnect() is called whenever a Myo has been connected.
-            @Override
-            public void onConnect(Myo myo, long timestamp) {
-                // Set the text color of the text view to cyan when a Myo connects.
-                life.setText("Connected");
-                mTextView.setTextColor(Color.CYAN);
-            }
-
-            // onDisconnect() is called whenever a Myo has been disconnected.
-            @Override
-            public void onDisconnect(Myo myo, long timestamp) {
-                // Set the text color of the text view to red when a Myo disconnects.
-                life.setTextColor(Color.RED);
-            }
-
-            // onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
-            // arm. This lets Myo know which arm it's on and which way it's facing.
-            @Override
-            public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
-                //mTextView.setText(myo.getArm() == Arm.LEFT ? R.string.arm_left : R.string.arm_right);
-            }
-
-            // onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
-            // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
-            // when Myo is moved around on the arm.
-            @Override
-            public void onArmUnsync(Myo myo, long timestamp) {
-                life.setText(R.string.hello_world);
-            }
-
-            // onUnlock() is called whenever a synced Myo has been unlocked. Under the standard locking
-            // policy, that means poses will now be delivered to the listener.
-            @Override
-            public void onUnlock(Myo myo, long timestamp) {
-                life.setText("unlocked");
-            }
-
-            // onLock() is called whenever a synced Myo has been locked. Under the standard locking
-            // policy, that means poses will no longer be delivered to the listener.
-            @Override
-            public void onLock(Myo myo, long timestamp) {
-                life.setText("locked");
-            }
-
-            // onOrientationData() is called whenever a Myo provides its current orientation,
-            // represented as a quaternion.
-            @Override
-            public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
-                // Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
-                float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
-                float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
-                float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
-
-                // Adjust roll and pitch for the orientation of the Myo on the arm.
-                if (myo.getXDirection() == XDirection.TOWARD_ELBOW) {
-                    roll *= -1;
-                    pitch *= -1;
-                }
-
-                // Next, we apply a rotation to the text view using the roll, pitch, and yaw.
-                mTextView.setRotation(roll);
-                mTextView.setRotationX(pitch);
-                mTextView.setRotationY(yaw);
-            }
-
-            // onPose() is called whenever a Myo provides a new pose.
-            @Override
-            public void onPose(Myo myo, long timestamp, Pose pose) {
-                //showToast(getString(R.string.pose, pose.toString()));
-            }
-        };
-    }
 
     private Timer updateTimer;
 
@@ -141,7 +68,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        context = this;
         updateTimer = new Timer();
         lifeCounter = 100;
         scoreCounter = 0;
@@ -153,20 +80,15 @@ public class MainActivity extends Activity {
 
         Game();
 
+        context = this;
 
-        mTextView = (TextView) findViewById(R.id.text);
+        createHub();
 
-        // First, we initialize the Hub singleton with an application identifier.
-        Hub hub = Hub.getInstance();
-        if (!hub.init(this, getPackageName())) {
-            // We can't do anything with the Myo device if the Hub can't be initialized, so exit.
-            Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
+        scanFordevice();
 
-        // Next, register for DeviceListener callbacks.
-        hub.addListener(mListener);
+        setLockPolicy();
+
+        ontSendData();
 
     }
 
@@ -216,15 +138,15 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-
+        Hub.getInstance().removeListener(mListener);
         BB8ConnectionActivity.getRobot().sleep();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        checkIfGameOver();
+        createAndAddListner();
+        //checkIfGameOver();
     }
 
     /**
@@ -413,15 +335,153 @@ public class MainActivity extends Activity {
     }
 
 
-    private void showToast(String text) {
-        Log.w(TAG, text);
-        if (mToast == null) {
-            mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
-        } else {
-            mToast.setText(text);
+
+    private void createHub() {
+        Toast.makeText(context, "attempting Hub creation.", Toast.LENGTH_SHORT).show();
+
+        Hub hub = Hub.getInstance();
+
+        if (!hub.init(this)) {
+
+            Toast.makeText(context, "Could not initialize the Hub.", Toast.LENGTH_SHORT).show();
+
+            Log.e(TAG, "Could not initialize the Hub.");
+
+            finish();
+
+            return;
+
         }
-        mToast.show();
+
     }
 
+
+
+    private void scanFordevice() {
+
+        Intent intent = new Intent(context, ScanActivity.class);
+
+        context.startActivity(intent);
+
+    }
+
+
+
+    private void setLockPolicy() {
+
+        Hub.getInstance().setLockingPolicy(Hub.LockingPolicy.NONE);
+
+    }
+
+
+
+    private void createAndAddListner() {
+
+
+
+        mListener = new AbstractDeviceListener() {
+
+            @Override
+
+            public void onConnect(Myo myo, long timestamp) {
+
+                Toast.makeText(context, "Myo Connected!", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+            @Override
+
+            public void onDisconnect(Myo myo, long timestamp) {
+
+                Toast.makeText(context, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+            @Override
+
+            public void onPose(Myo myo, long timestamp, Pose pose) {
+
+                switch (pose) {
+
+                    case REST:
+
+                        Toast.makeText(context, "REST", Toast.LENGTH_SHORT).show();
+                        if(BB8ConnectionActivity.getRobot() != null)
+                            BB8ConnectionActivity.getRobot().drive((float) 0, (float) 0);
+                        break;
+
+                    case FIST:
+
+                        Toast.makeText(context, "FIST", Toast.LENGTH_SHORT).show();
+                        if(BB8ConnectionActivity.getRobot() != null)
+                            BB8ConnectionActivity.getRobot().drive((float) 0, (float) 0);
+
+
+                        break;
+
+                    case WAVE_IN:
+
+                        Toast.makeText(context, "WAVE_IN", Toast.LENGTH_SHORT).show();
+                        if(BB8ConnectionActivity.getRobot() != null)
+                            BB8ConnectionActivity.getRobot().drive((float) 0, (float) 0);
+                        break;
+
+                    case WAVE_OUT:
+
+                        Toast.makeText(context, "WAVE_OUT", Toast.LENGTH_SHORT).show();
+                        if(BB8ConnectionActivity.getRobot() != null)
+                            BB8ConnectionActivity.getRobot().drive((float) 0, (float) 0);
+                        break;
+
+                    case FINGERS_SPREAD:
+
+                        Toast.makeText(context, "FINGERS_SPREAD", Toast.LENGTH_SHORT).show();
+                        if(BB8ConnectionActivity.getRobot() != null)
+                            BB8ConnectionActivity.getRobot().drive((float) 0, (float) 100);
+
+                        break;
+
+                    case DOUBLE_TAP:
+
+                        Toast.makeText(context, "DOUBLE_TAP", Toast.LENGTH_SHORT).show();
+                        if(BB8ConnectionActivity.getRobot() != null)
+                            BB8ConnectionActivity.getRobot().drive((float) 0, (float) 0);
+                        break;
+
+                    case UNKNOWN:
+
+                        Toast.makeText(context, "UNKNOWN", Toast.LENGTH_SHORT).show();
+
+                        break;
+
+                }
+
+            }
+
+        };
+
+
+
+        Hub.getInstance().addListener(mListener);
+
+    }
+
+
+
+
+
+    private void ontSendData() {
+
+        if (Hub.getInstance().isSendingUsageData()) {
+
+            Hub.getInstance().setSendUsageData(false);
+
+        }
+
+    }
 
 }
